24fd4f27b39e548d91ba804c689a8f863fa953fc
DAN BEN TOV <danbentov3@gmail.com>
2024-11-15 13:37:04 +0200

    viw

--- MyGarageFinderApp/Views/MyCarsProfileView.xaml.cs
+++ MyGarageFinderApp/Views/MyCarsProfileView.xaml.cs
@@ -1,9 +1,12 @@
+using MyGarageFinderApp.ViewModels;
+
 namespace MyGarageFinderApp.Views;
 
 public partial class MyCarsProfileView : ContentPage
 {
-	public MyCarsProfileView()
+	public MyCarsProfileView(MyCarsProfileViewModel vm)
 	{
+		this.BindingContext = vm;
 		InitializeComponent();
 	}
 }
